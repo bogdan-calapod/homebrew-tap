fb9088f55e5ce80ffa83ec9d95ab986648983bc5
Bogdan Calapod <bogdan.calapod@pdq.com>
2026-03-21 09:46:18 +0200

    task: Bump ytm

--- Formula/ncytm.rb
+++ Formula/ncytm.rb
@@ -1,8 +1,8 @@
 class Ncytm < Formula
   desc "YouTube Music client for the terminal"
   homepage "https://github.com/bogdan-calapod/ncytm"
-  url "https://github.com/bogdan-calapod/ncytm/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "b73b5a0b9bbbc89e3419a1a48ef059d43bc92336db34eb80e509ee03a09b2ee9"
+  url "https://github.com/bogdan-calapod/ncytm/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "8d2fb1cbb7b665c487922a34359b83d7c34f338fdee8159015e96ff7a1fe53ab"
   license "BSD-2-Clause"
 
   depends_on "rust" => :build
